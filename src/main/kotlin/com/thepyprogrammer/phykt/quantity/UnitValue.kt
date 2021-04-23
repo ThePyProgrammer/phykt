e9d454fca6fc0ce9f54a40b8c61dcff78a1e9b24
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-23 16:53:44 +0800

    Update UnitValue.kt and Create UnitValueTest.kt

--- src/main/kotlin/com/thepyprogrammer/phykt/quantity/UnitValue.kt
+++ src/main/kotlin/com/thepyprogrammer/phykt/quantity/UnitValue.kt
@@ -1,7 +1,5 @@
 package com.thepyprogrammer.phykt.quantity
 
-import java.text.NumberFormat
-import java.util.*
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 import kotlin.collections.HashMap
@@ -12,181 +10,259 @@ import kotlin.math.pow
 open class UnitValue(var value: Double = 0.0, var unit: String = "") : Cloneable, Comparable<Any?> {
     private lateinit var powers: HashMap<String, Double>
 
-    constructor(uv: UnitValue) : this(uv.value, uv.unit) {}
+    constructor(uv: UnitValue) : this(uv.value, uv.unit)
 
-    public override fun clone(): UnitValue {
-        return UnitValue(this)
+    init {
+        fix(unit)
     }
 
-    fun add(vararg uvs: UnitValue): UnitValue {
-        var `val` = value
-        for (uv in uvs) {
-            if (uv.unit == unit) `val` += uv.value
-        }
-        return UnitValue(`val`, unit)
+    // unary operator funcs
+    operator fun unaryPlus() = this
+
+    operator fun unaryMinus() = UnitValue(-value, unit)
+
+
+
+    infix operator fun plus(other: UnitValue) = run {
+        if(other.unit == unit) UnitValue(other.value + value, unit)
+        else this
     }
 
-    fun iadd(vararg uvs: UnitValue) {
-        var `val` = value
-        for (uv in uvs) {
-            if (uv.unit == unit) `val` += uv.value
-        }
-        value = `val`
+    infix operator fun plus(other: Double) = UnitValue(value + other, unit)
+
+    infix operator fun plus(other: Float) = UnitValue(value + other, unit)
+
+    infix operator fun plus(other: Int) = UnitValue(value + other, unit)
+
+    infix operator fun plus(other: Short) = UnitValue(value + other, unit)
+
+    infix operator fun plus(other: Long) = UnitValue(value + other, unit)
+
+    infix operator fun plusAssign(other: UnitValue) {
+        if(other.unit == unit) value +=other.value
     }
 
-    fun sub(vararg uvs: UnitValue): UnitValue {
-        var `val` = value
-        for (uv in uvs) {
-            if (uv.unit == unit) `val` -= uv.value
-        }
-        return UnitValue(`val`, unit)
+    infix operator fun plusAssign(other: Double) {
+        value += other
     }
 
-    fun isub(vararg uvs: UnitValue) {
-        var `val` = value
-        for (uv in uvs) {
-            if (uv.unit == unit) `val` -= uv.value
-        }
-        value = `val`
+    infix operator fun plusAssign(other: Float) {
+        value += other
     }
 
-    fun subtract(uv: UnitValue?): UnitValue {
-        return sub(uv!!)
+    infix operator fun plusAssign(other: Int) {
+        value += other
     }
 
-    fun mul(vararg uvs: UnitValue): UnitValue {
-        var numer = deriveNumer()
-        var denom = deriveDenom()
-        var `val` = value
-        for (uv in uvs) {
-            `val` *= uv.value
-            numer += uv.deriveNumer()
-            denom += uv.deriveDenom()
-        }
-        return UnitValue(`val`, formUnit(numer, denom))
+    infix operator fun plusAssign(other: Short) {
+        value += other
     }
 
-    fun multiply(uv: UnitValue?): UnitValue {
-        return mul(uv!!)
+    infix operator fun plusAssign(other: Long) {
+        value += other
     }
 
-    operator fun div(uv: UnitValue): UnitValue {
-        var numer = deriveNumer()
-        var denom = deriveDenom()
-        val `val` = value / uv.value
-        denom += uv.deriveNumer()
-        numer += uv.deriveDenom()
-        return UnitValue(`val`, formUnit(numer, denom))
+
+
+    infix operator fun minus(other: UnitValue) = run {
+        if(other.unit == unit) UnitValue(other.value - value, unit)
+        else this
     }
 
-    fun divide(uv: UnitValue): UnitValue {
-        return div(uv)
+    infix operator fun minus(other: Double) = UnitValue(value - other, unit)
+
+    infix operator fun minus(other: Float) = UnitValue(value - other, unit)
+
+    infix operator fun minus(other: Int) = UnitValue(value - other, unit)
+
+    infix operator fun minus(other: Short) = UnitValue(value - other, unit)
+
+    infix operator fun minus(other: Long) = UnitValue(value - other, unit)
+
+    infix operator fun minusAssign(other: UnitValue) {
+        if(other.unit == unit) value -=other.value
     }
+
+    infix operator fun minusAssign(other: Double) {
+        value -= other
+    }
+
+    infix operator fun minusAssign(other: Float) {
+        value -= other
+    }
+
+    infix operator fun minusAssign(other: Int) {
+        value -= other
+    }
+
+    infix operator fun minusAssign(other: Short) {
+        value -= other
+    }
+
+    infix operator fun minusAssign(other: Long) {
+        value -= other
+    }
+
+
+
+
+    infix operator fun times(other: UnitValue) = run {
+        val numer = deriveNumer() + other.deriveNumer()
+        val denom = deriveDenom() + other.deriveDenom()
+        val `val` = value * other.value
+        UnitValue(`val`, formUnit(numer, denom))
+    }
+
+    infix operator fun times(other: Double) = UnitValue(value * other, unit)
+
+    infix operator fun times(other: Float) = UnitValue(value * other, unit)
+
+    infix operator fun times(other: Int) = UnitValue(value * other, unit)
+
+    infix operator fun times(other: Short) = UnitValue(value * other, unit)
+
+    infix operator fun times(other: Long) = UnitValue(value * other, unit)
+
+    infix operator fun timesAssign(other: Double) {
+        value *= other
+    }
+
+    infix operator fun timesAssign(other: Float) {
+        value *= other
+    }
+
+    infix operator fun timesAssign(other: Int) {
+        value *= other
+    }
+
+    infix operator fun timesAssign(other: Short) {
+        value *= other
+    }
+
+    infix operator fun timesAssign(other: Long) {
+        value *= other
+    }
+
+
+
+
+    infix operator fun div(other: UnitValue) = run {
+        val numer = deriveNumer() + other.deriveDenom()
+        val denom = deriveDenom() + other.deriveNumer()
+        val `val` = value / other.value
+        UnitValue(`val`, formUnit(numer, denom))
+    }
+
+    infix operator fun div(other: Double) = UnitValue(value / other, unit)
+
+    infix operator fun div(other: Float) = UnitValue(value / other, unit)
+
+    infix operator fun div(other: Int) = UnitValue(value / other, unit)
+
+    infix operator fun div(other: Short) = UnitValue(value / other, unit)
+
+    infix operator fun div(other: Long) = UnitValue(value / other, unit)
+
+    infix operator fun divAssign(other: Double) {
+        value /= other
+    }
+
+    infix operator fun divAssign(other: Float) {
+        value /= other
+    }
+
+    infix operator fun divAssign(other: Int) {
+        value /= other
+    }
+
+    infix operator fun divAssign(other: Short) {
+        value /= other
+    }
+
+    infix operator fun divAssign(other: Long) {
+        value /= other
+    }
+
+
+
+
+
+    infix operator fun rem(other: UnitValue) = run {
+        if(other.unit == unit) UnitValue(other.value % value, unit)
+        else this
+    }
+
+    infix operator fun rem(other: Double) = UnitValue(value % other, unit)
+
+    infix operator fun rem(other: Float) = UnitValue(value % other, unit)
+
+    infix operator fun rem(other: Int) = UnitValue(value % other, unit)
+
+    infix operator fun rem(other: Short) = UnitValue(value % other, unit)
+
+    infix operator fun rem(other: Long) = UnitValue(value % other, unit)
+
+    infix operator fun remAssign(other: Double) {
+        value %= other
+    }
+
+    infix operator fun remAssign(other: Float) {
+        value %= other
+    }
+
+    infix operator fun remAssign(other: Int) {
+        value %= other
+    }
+
+    infix operator fun remAssign(other: Short) {
+        value %= other
+    }
+
+    infix operator fun remAssign(other: Long) {
+        value %= other
+    }
+
+
+    operator fun inc() = this plus 1
+    operator fun dec() = this minus 1
 
     fun pow(pow: Double): UnitValue {
         var numer = ""
         var denom = ""
         for ((key, value1) in powers) {
             val p = value1 * pow
-            if (p == 0.0) continue else if (p == 1.0) numer += key else if (p == -1.0) denom += key else if (p > 0) numer += "$key^$p" else if (p < 0) denom += "$key^" + abs(
-                p
-            )
+            if (p == 0.0)
+                continue
+            else if (p == 1.0)
+                numer += key
+            else if (p == -1.0)
+                denom += key
+            else if (p > 0)
+                numer += "$key^$p"
+            else if (p < 0)
+                denom += "$key^" + abs(p)
         }
         return UnitValue(value.pow(pow), formUnit(numer, denom))
     }
 
-    fun negate(): UnitValue {
-        return UnitValue(-value, unit)
-    }
+    fun pow(pow: Float) = pow(pow.toDouble())
+    fun pow(pow: Int) = pow(pow.toDouble())
+    fun pow(pow: Short) = pow(pow.toDouble())
+    fun pow(pow: Long) = pow(pow.toDouble())
+    fun pow(pow: UnitValue) = pow(pow.toDouble())
 
-    private fun deriveNumer(): String {
-        if (!unit.contains("/")) return unit
-        return if (unit.startsWith("1/")) "" else unit.split("/").toTypedArray()[0]
-    }
 
-    private fun deriveDenom(): String {
-        return if (!unit.contains("/")) "" else unit.split("/").toTypedArray()[1]
-    }
 
-//    private fun classify(uv: UnitValue): UnitValue {
-//        if (uv.unit == "kgm^2/s^3") return Power(uv.value)
-//        return if (uv.unit == "m/s^2") Acceleration(uv.value) else uv
-//    }
+    fun toDouble() = value.toInt()
+    fun toFloat() = value.toInt()
+    fun toInt() = value.toInt()
+    fun toShort() = value.toInt().toShort()
+    fun toLong() = value.toLong()
 
-    private fun fix(unit: String?): String {
-        var numer = ""
-        var denom = ""
-        var ognum = unit
-        var ogden = ""
-        if (unit!!.contains("/")) {
-            val temp = unit.split("/").toTypedArray()
-            ognum = temp[0]
-            ogden = temp[1]
-        }
-        powers = HashMap()
-        var m: Matcher
-        for (u in SIs) {
-            var p = 0.0
-            // Pattern re = Pattern.compile(String.format("(%s\\^)(-?\\d+(\\.\\d+)?)", u));
-            var start: Int
-            while (ognum!!.contains("$u^")) {
-                start = ognum.indexOf(u + "")
-                var str = ""
-                for (i in ognum.substring(start + u.length + 1).toCharArray()) {
-                    if (Pattern.compile("\\d|\\.").matcher("" + i).also { m = it }
-                            .matches()) str += m.group() else break
-                }
-                p += if (str.contains(".")) str.toDouble() else str.toInt().toDouble()
-                ognum = ognum.replaceFirst(u + "\\^" + str.toRegex(), "")
-            }
-            while (ognum!!.contains(u)) {
-                p++
-                ognum = ognum.replaceFirst(u.toRegex(), "")
-            }
-            while (ogden.contains("$u^")) {
-                start = ogden.indexOf(u + "")
-                var str = ""
-                for (i in ogden.substring(start + u.length + 1).toCharArray()) {
-                    if (Pattern.compile("\\d|\\.").matcher("" + i).also { m = it }
-                            .matches()) str += m.group() else break
-                }
-                p -= if (str.contains(".")) str.toDouble() else str.toInt().toDouble()
-                ogden = ogden.replaceFirst(u + "\\^" + str.toRegex(), "")
-            }
-            while (ogden.contains(u)) {
-                p--
-                ogden = ogden.replaceFirst(u.toRegex(), "")
-            }
-            powers[u] = p
-        }
-        for ((key, value1) in powers) {
-            if (value1 == 0.0) continue else if (value1 == 1.0) numer += key else if (value1 == -1.0) denom += key else if (value1 > 0) numer += "$key^$value1" else if (value1 < 0) denom += "$key^" + abs(
-                value1
-            )
-        }
-        return formUnit(numer, denom)
-    }
 
-    fun removeRandomZeroes(string: String): String {
-        var str = string
-        if (str.contains(".")) {
-            while (str[str.length - 1] == '0') {
-                str = str.substring(0, str.length - 1)
-            }
-            if (str[str.length - 1] == '.') {
-                str = str.substring(0, str.length - 1)
-            }
-        }
-        return str.trim { it <= ' ' }
-    }
 
-    override fun toString(): String {
-        return "$value $unit"
-    }
-
-    fun toString(locale: Locale?): String {
-        return NumberFormat.getNumberInstance().format(value) + " " + unit
-    }
+    // Boolean Functions
+    operator fun not() = value == 0.0
 
     fun equals(other: UnitValue): Boolean {
         return value == other.value && unit == other.unit
@@ -197,9 +273,9 @@ open class UnitValue(var value: Double = 0.0, var unit: String = "") : Cloneable
         else false
     }
 
-    operator fun compareTo(o: UnitValue): Int {
-        if (value > o.value) return 1
-        return if (value == o.value) 0 else -1
+    operator fun compareTo(uv: UnitValue): Int {
+        if (value > uv.value) return 1
+        return if (value == uv.value) 0 else -1
     }
 
     override operator fun compareTo(other: Any?): Int {
@@ -208,16 +284,98 @@ open class UnitValue(var value: Double = 0.0, var unit: String = "") : Cloneable
         } else -1
     }
 
-    companion object {
-        private val SIs = arrayOf("kg", "mol", "m", "A", "s", "K", "cd")
-        private fun deriveNumer(unit: String): String {
-            if (!unit.contains("/")) return unit
-            return if (unit.startsWith("1/")) "" else unit.split("/").toTypedArray()[0]
+
+
+
+
+    private fun deriveNumer(): String {
+        if (!unit.contains("/")) return unit
+        return if (unit.startsWith("1/")) "" else unit.split("/").toTypedArray()[0]
+    }
+
+    private fun deriveDenom(): String {
+        return if (!unit.contains("/")) "" else unit.split("/").toTypedArray()[1]
+    }
+
+
+    private fun fix(unit: String): String {
+        var numer = ""
+        var denom = ""
+        var ognum = unit
+        var ogden = ""
+        if (unit.contains("/")) {
+            val temp = unit.split("/").toTypedArray()
+            ognum = temp[0]
+            ogden = temp[1]
+        }
+        powers = hashMapOf()
+        for (u in SIs) {
+            val (pownum, tempnum) = extractAll(ognum, u)
+            val (powden, tempden) = extractAll(ogden, u)
+
+            ognum = tempnum
+            ogden = tempden
+
+            powers[u] = pownum - powden
         }
 
-        private fun deriveDenom(unit: String): String {
-            return if (!unit.contains("/")) "" else unit.split("/").toTypedArray()[0]
+        loop@ for ((key, value1) in powers) {
+            when {
+                value1 == 0.0 -> continue@loop
+                value1 == 1.0 -> numer += key
+                value1 == -1.0 -> denom += key
+                value1 > 0 -> numer += "$key^$value1"
+                value1 < 0 -> denom += "$key^" + abs(value1)
+            }
         }
+        return formUnit(numer, denom)
+    }
+
+    private fun extractAll(template: String, codon: String) = run {
+        var m: Matcher
+        var p = 0.0
+        // Pattern re = Pattern.compile(String.format("(%s\\^)(-?\\d+(\\.\\d+)?)", u));
+        var start: Int
+        var temp = template
+        while (temp.contains("$codon^")) {
+            start = temp.indexOf("$codon^")
+            var str = ""
+            for (i in temp.substring(start + codon.length + 1).toCharArray()) {
+                if (Pattern.compile("\\d|\\.").matcher("" + i).also { m = it }
+                        .matches()) str += m.group() else break
+            }
+            p += if (str.contains(".")) str.toDouble() else str.toInt().toDouble()
+            temp = temp.replaceFirst("$codon\\^" + str.toRegex(), "")
+        }
+        while (temp.contains(codon)) {
+            p++
+            temp = temp.replaceFirst(codon.toRegex(), "")
+        }
+        ExtractType(p, temp)
+    }
+
+    private data class ExtractType(val p: Double, val temp: String)
+
+
+    public override fun clone(): UnitValue {
+        return UnitValue(this)
+    }
+
+
+    override fun toString(): String {
+        return "$value $unit"
+    }
+
+    override fun hashCode(): Int {
+        var result = value.hashCode()
+        result = 31 * result + unit.hashCode()
+        result = 31 * result + powers.hashCode()
+        return result
+    }
+
+
+    companion object {
+        private val SIs = arrayOf("kg", "mol", "m", "A", "s", "K", "cd")
 
         private fun formUnit(numer: String, denom: String) = run {
             if (numer.isNotEmpty() && denom.isNotEmpty())
@@ -229,5 +387,4 @@ open class UnitValue(var value: Double = 0.0, var unit: String = "") : Cloneable
             else ""
         }
     }
-
 }
