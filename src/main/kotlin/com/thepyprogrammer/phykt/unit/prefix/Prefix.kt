0a5444c154d8a47c90d66037ac5933e28de1aa6c
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-23 21:39:04 +0800

    Create Prefix

---
+++ src/main/kotlin/com/thepyprogrammer/phykt/unit/prefix/Prefix.kt
@@ -0,0 +1,3 @@
+package com.thepyprogrammer.phykt.unit.prefix
+
+data class Prefix(val prefix: String = "", val name: String = "", val power: Double = 1.0)
---
+++ src/main/kotlin/com/thepyprogrammer/phykt/unit/prefix/Prefixes.kt
@@ -0,0 +1,25 @@
+package com.thepyprogrammer.phykt.unit.prefix
+
+val prefixes = mutableListOf(
+    Prefix(),
+    Prefix("y", "yocto", 1e-24),
+    Prefix("z", "zepto", 1e-21),
+    Prefix("a", "atto", 1e-18),
+    Prefix("f", "femto", 1e-15),
+    Prefix("p", "pico", 1e-12),
+    Prefix("n", "nano", 1e-9),
+    Prefix("μ", "micro", 1e-6),
+    Prefix("m", "milli", 1e-3),
+    Prefix("c", "centi", 1e-2),
+    Prefix("d", "deci", 1e-1),
+    Prefix("da", "deka", 1e1),
+    Prefix("h", "hecto", 1e2),
+    Prefix("k", "kilo", 1e3),
+    Prefix("M", "Mega", 1e6),
+    Prefix("G", "Giga", 1e9),
+    Prefix("T", "Tera", 1e12),
+    Prefix("P", "Peta", 1e15),
+    Prefix("E", "Exa", 1e18),
+    Prefix("Z", "Zeta", 1e21),
+    Prefix("Y", "Yota", 1e24)
+)
