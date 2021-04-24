0d43670f2744ae9e56be6e4d5b3c019ff8876e0b
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-24 19:59:30 +0800

    Add 3D and Vector Support to Point.kt

--- src/main/kotlin/com/thepyprogrammer/phykt/base/Point.kt
+++ src/main/kotlin/com/thepyprogrammer/phykt/base/Point.kt
@@ -1,22 +1,30 @@
 package com.thepyprogrammer.phykt.base
 
+import com.thepyprogrammer.phykt.spatial.SpatialVector
 import kotlin.math.sqrt
 
 
-data class Point(var x: Double = 0.0, var y: Double = 0.0): Cloneable {
-    constructor(): this(0.0, 0.0)
+data class Point(var x: Double = 0.0, var y: Double = 0.0, var z: Double = 0.0): Cloneable {
 
-    operator fun set(x: Double, y: Double) {
+    fun set(x: Double) {
+        this.x = x
+    }
+    fun set(x: Double, y: Double) {
         this.x = x
         this.y = y
     }
+    fun set(x: Double, y: Double, z: Double) {
+        this.x = x
+        this.y = y
+        this.z = z
+    }
 
     override fun toString(): String {
-        return "($x, $y)"
+        return "($x, $y, $z)"
     }
 
     fun copy(): Point {
-        return Point(x, y)
+        return Point(x, y, z)
     }
 
     override fun clone(): Point {
@@ -27,15 +35,19 @@ data class Point(var x: Double = 0.0, var y: Double = 0.0): Cloneable {
     }
 
     operator fun compareTo(p: Point): Int {
-        return if (x != p.x) (x - p.x).toInt() else (y - p.y).toInt()
+        return when {
+            x != p.x -> (x - p.x).toInt()
+            y != p.y -> (y - p.y).toInt()
+            else -> (z - p.z).toInt()
+        }
     }
 
     fun fromOrigin(o: Point): Point {
-        return Point(x - o.x, y - o.y)
+        return Point(x - o.x, y - o.y, z - o.z)
     }
 
     fun getHypotenuse(): Double {
-        return sqrt(x * x + y * y)
+        return sqrt(x * x + y * y + z * z)
     }
 
     fun hypotenuseFrom(p: Point): Double {
@@ -44,8 +56,14 @@ data class Point(var x: Double = 0.0, var y: Double = 0.0): Cloneable {
 
 
     // Instance methods desugar to prototype methods, eg. Point.prototype.moveBy = function(dx, dy) {}
-    fun moveBy(dx: Double, dy: Double) {
+    fun moveBy(dx: Double, dy: Double, dz: Double) {
         this.x += dx
         this.y += dy
+        this.z += dz
+    }
+
+    fun vectorTo(p: Point) = run {
+        val (x, y, z) = p.fromOrigin(this)
+        SpatialVector(x, y, z)
     }
 }
