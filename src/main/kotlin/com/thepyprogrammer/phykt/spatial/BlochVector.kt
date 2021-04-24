4ce3dae14393ff825afaf923976da4e7e2777a86
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-24 20:00:39 +0800

    Update BlochVector.kt

--- src/main/kotlin/com/thepyprogrammer/phykt/spatial/BlochVector.kt
+++ src/main/kotlin/com/thepyprogrammer/phykt/spatial/BlochVector.kt
@@ -1,7 +1,19 @@
 package com.thepyprogrammer.phykt.spatial
 
+import com.thepyprogrammer.phykt.base.Angle
 import com.thepyprogrammer.phykt.linalg.Vector
 
-open class BlochVector(): Vector() {
-    // TODO
+/**
+ * TODO
+ */
+open class BlochVector(
+    val radial: Double = 1.0,
+    val polar: Angle = Angle(),
+    val azimuthal: Angle = Angle()
+): Vector() {
+
+
+    fun getCoords(theta: Angle, phi: Angle) {
+
+    }
 }
