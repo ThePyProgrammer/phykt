f827e17c89cb1354205b937017994959f4fbf691
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-24 19:59:14 +0800

    Add Default Constructor to Angle.kt

--- src/main/kotlin/com/thepyprogrammer/phykt/base/Angle.kt
+++ src/main/kotlin/com/thepyprogrammer/phykt/base/Angle.kt
@@ -1,6 +1,7 @@
 package com.thepyprogrammer.phykt.base
 
-data class Angle(val angle: Double) {
+data class Angle(val angle: Double = 0.0) {
+
     infix operator fun plus(x: Angle) = Angle(angle + x.angle)
     infix operator fun plus(x: Double) = Angle(angle + x)
     infix operator fun plus(x: Float) = Angle(angle + x.toDouble())
