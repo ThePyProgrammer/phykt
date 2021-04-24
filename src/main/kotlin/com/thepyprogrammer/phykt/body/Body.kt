65279ad4d9eeec4e968df7117591a383f5501935
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-25 01:40:14 +0800

    Create Body.kt

---
+++ src/main/kotlin/com/thepyprogrammer/phykt/body/Body.kt
@@ -0,0 +1,12 @@
+package com.thepyprogrammer.phykt.body
+
+import com.thepyprogrammer.phykt.types.Mass
+import com.thepyprogrammer.phykt.types.Momentum
+import com.thepyprogrammer.phykt.unit.kg
+import com.thepyprogrammer.phykt.unit.m
+import com.thepyprogrammer.phykt.unit.s
+
+class Body {
+    private val mass: Mass = Mass(1.0)
+    private val momentum: Momentum = Momentum(1.0)
+}
