d64cc44b9c94df62c88f5e8977537ef13ac55a6c
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-24 20:00:21 +0800

    Create some utils for linalg subpackage

--- src/main/kotlin/com/thepyprogrammer/phykt/linalg/LinAlgUtils.kt
+++ src/main/kotlin/com/thepyprogrammer/phykt/linalg/LinAlgUtils.kt
@@ -1,28 +1,37 @@
 package com.thepyprogrammer.phykt.linalg
 
+import com.thepyprogrammer.ktlib.array.DoubleArray2D
+import com.thepyprogrammer.ktlib.array.each
 import com.thepyprogrammer.ktlib.array.zeros
 
 fun vectorOf(vararg vals: Double) = Vector(*vals)
 
-fun columnVectorOf(vararg vals: Double) = run {
-    val list = mutableListOf<Vector>()
-    vals.forEach {
-        list.add(vectorOf(it))
-    }
-    Matrix(*list.toTypedArray())
-}
+fun columnVectorOf(vararg vals: Double) = vals.toTypedArray().toColumnVector()
 
-fun vectorArrayOf(arr: Array<Array<Double>>) = run {
-    val list = mutableListOf<Vector>()
-    arr.forEach {
-        list.add(vectorOf(*it.toDoubleArray()))
-    }
-    list.toTypedArray()
-}
+fun vectorArrayOf(arr: DoubleArray2D): VectorArray = arr.vectorize()
 
 fun matrixOf(vararg rows: Vector) = Matrix(*rows)
+
+fun matrixOf(rows: List<Vector>) = Matrix(*rows.toTypedArray())
+
+fun matrixOf(matrix: DoubleArray2D) = matrixOf(*matrix.vectorize())
 
 fun zeroMatrixOf(m: Int = 2, n: Int = 2) = run {
     val array = vectorArrayOf(zeros(m, n))
     Matrix(*array)
 }
+
+typealias VectorArray = Array<Vector>
+
+fun DoubleArray.toVector(): Vector = vectorOf(*this)
+fun Array<Double>.toVector(): Vector = toDoubleArray().toVector()
+
+
+fun Array<Double>.toColumnVector(): Matrix = matrixOf(
+    *each { vectorOf(it) }
+)
+
+fun DoubleArray.toColumnVector(): Matrix = toTypedArray().toColumnVector()
+
+fun DoubleArray2D.vectorize(): Array<Vector> = each { it.toVector() }
+
