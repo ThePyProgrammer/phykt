bc0f4ec5281bba848c5e33fdc5ef30eaca744795
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-29 14:24:57 +0800

    Add important physical properties to Body.kt

--- src/main/kotlin/com/thepyprogrammer/phykt/body/Body.kt
+++ src/main/kotlin/com/thepyprogrammer/phykt/body/Body.kt
@@ -1,12 +1,23 @@
 package com.thepyprogrammer.phykt.body
 
-import com.thepyprogrammer.phykt.types.Mass
-import com.thepyprogrammer.phykt.types.Momentum
+import com.thepyprogrammer.phykt.types.*
 import com.thepyprogrammer.phykt.unit.kg
 import com.thepyprogrammer.phykt.unit.m
 import com.thepyprogrammer.phykt.unit.s
 
 class Body {
-    private val mass: Mass = Mass(1.0)
-    private val momentum: Momentum = Momentum(1.0)
+    val mass: Mass = Mass(1.0)
+
+    val position: Distance = Distance(0.0)
+    val velocity: Velocity = Velocity(0.0)
+    val acceleration: Acceleration = Acceleration(0.0)
+
+    val momentum: Momentum
+        get() = (mass * velocity) as Momentum
+
+    val netForce: Force
+        get() = (mass * acceleration) as Force
+
+    val kineticEnergy: Energy
+        get() = (mass * velocity.pow(2) / 2) as Energy
 }
