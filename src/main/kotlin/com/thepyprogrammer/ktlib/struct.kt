71fac698182b8b2573442e7a2d7525e9ed25abea
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-05-24 16:14:22 +0800

    Add anystruct as an additional function to struct.kt

--- src/main/kotlin/com/thepyprogrammer/ktlib/struct.kt
+++ src/main/kotlin/com/thepyprogrammer/ktlib/struct.kt
@@ -17,8 +17,15 @@ class Object<T, R>(vararg vals: Pair<T, R>): HashMap<T, R>(hashMapOf(*vals)) {
  */
 fun <T, R> struct(vararg vals: Pair<T, R>, apply: Object<T, R>.() -> Unit = {}) = Object(*vals).apply(apply)
 
+
+/**
+ * This function exists to allow the proper reading of values so that the struct, when formed, does not backfire
+ * Generalises all keys as of type Any. It allows the let function to work much better.
+ */
+fun anystruct(vararg vals: Pair<Any, Any>, apply: Object<Any, Any>.() -> Unit = {}) = Object(*vals).apply(apply)
+
 fun test() {
-    var other: Int = 5
+    var other = 5
     val obj = struct(
         "five" `=` 10,
         "hello" `=` 99,
